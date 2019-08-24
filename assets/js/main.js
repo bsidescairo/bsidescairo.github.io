a9d0a92ae717d508a9378aa76538fde77e158a2a
Amgad <26678864+bluzr@users.noreply.github.com>
2019-08-24 14:54:17 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,7 +20,7 @@
     /* ==========================================================================
        countdown timer
        ========================================================================== */
-     jQuery('#clock').countdown('2019/2/2',function(event){
+     jQuery('#clock').countdown('2020/2/15',function(event){
       var $this=jQuery(this).html(event.strftime(''
       +'<div class="time-entry days"><span>%-D</span> Days</div> '
       +'<div class="time-entry hours"><span>%H</span> Hours</div> '
